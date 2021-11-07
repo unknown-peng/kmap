f06d95b6fd554d06516f08a57d34e5b65f47fe6b
Shilu Tudu <shilutudu123@gmail.com>
2021-11-07 21:50:39 +0530

    added project

---
+++ main.js
@@ -0,0 +1,27 @@
+var inpt_num = document.getElementById('n_input')
+var minterm = document.getElementById('minterms')
+var dntcare = document.getElementById('dntcare')
+// const ktable = document.getElementById("kmap")
+
+// form.addEventListener('submit', (e) => {
+//     // let messages = []
+//     if (inpt_num.value == '4') {
+//         var x = document.getElementById("n_input");
+//         if (x.style.display === "none") {
+//             x.style.display = "block";
+//         } else {
+//             x.style.display = "none";
+//         }
+//     }
+// })
+
+
+function myFunction() {
+    // var x = document.getElementById("kmap");
+    // if (n_input.value == '4')
+    //     if (x.style.display === "none") {
+    //         x.style.display = "block";
+    //     } else {
+    //         x.style.display = "none";
+    //     }
+}
